aa69690ae9950628f83d508a2c74fad7ae0dd489
Silviaatanes <95342339+Silviaatanes@users.noreply.github.com>
2022-03-17 11:01:11 +0100

    Update calculos.js

--- js/calculos.js
+++ js/calculos.js
@@ -19,3 +19,53 @@ function factorial() {
 
     return factorial;
 }
+
+// Definir variables
+var n = 0;
+
+// Funcion para leer numeros Reales
+function leerNumero() {
+    num = parseInt(
+        prompt("Porfi, escribe un número")
+    );
+    return num;
+}
+
+function mostrarResultados(nf) {
+    document.getElementById('td21').innerHTML = n;
+    document.getElementById('td22').innerHTML = factorial(nf);
+}
+function mostrarfactorial (nf ) {
+    document.getElementById('td21').innerHTML = nf;
+    document.getElementById('td22').innerHTML = factorial ( nf) ;
+}
+function mostrarMultiplos(n, m) {
+    document.getElementById('td11').innerHTML = n;
+    document.getElementById('td12').innerHTML = m;
+    document.getElementById('td13').innerHTML = multiplosDeHasta(n,m);
+}
+function multiplosDeHasta() {
+    mensaje = "Los múltiplo de "
+        + n +
+        " hasta " + m +
+        " son: ";
+
+    multiplo = 0;
+    for (i=1; multiplo < m; i++) {
+        console.log(i, '',  multiplo);
+        multiplo = n*i;
+        mensaje = mensaje + multiplo + " ";
+    }
+
+    return mensaje;
+}
+
+function factorial( nf ) {
+    var factorial = 1;
+    for ( i=1; i <= nf; i++ ) {
+        factorial = factorial * i;
+    }
+
+    return factorial;
+}
+
